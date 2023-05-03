e3dace117749c47ae3d66e4bd23950cae4b20863
Duracyq <szymon.kojek@wp.pl>
2023-05-03 12:12:27 +0200

    added basic UI and JokeAPI functioning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  const url = 'https://v2.jokeapi.dev/joke/Any'
+
+  const [jokes, setJokes] = useState([])
+  
+  const [isLoading, setIsLoading] = useState(false)
+
+  const newJoke = async () => {
+    setIsLoading(true);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data); // Log the response to the console
+    setJokes((prev) => [data, ...prev]);
+    setIsLoading(false);
+  }
+
+  const removeJoke = (id) => {
+    setJokes((prev) => prev.filter((element) => element.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          stronka :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <button onClick={newJoke}>Joke</button>
+      <ul>
+        {jokes.map((joke) => (
+          <li key={joke.id}>
+            {joke.setup} {joke.delivery}
+            <button onClick={() => removeJoke(joke.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>  
     </div>
   );
 }
